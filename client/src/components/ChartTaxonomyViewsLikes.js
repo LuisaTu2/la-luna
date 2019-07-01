350bdfa8319d00bc433802cf70d3df43cb4f82d5
LuisaP <luisa.pegoraro@gmail.com>
2019-07-01 17:10:07 -0400

    styling and pagination2

--- client/src/components/ChartTaxonomyViewsLikes.js
+++ client/src/components/ChartTaxonomyViewsLikes.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import {VictoryPie, VictoryLegend} from 'victory';
+import {VictoryPie, VictoryLegend, VictoryTooltip} from 'victory';
 
 
 class ChartTaxonomyViewsLikes extends Component {
@@ -13,22 +13,26 @@ class ChartTaxonomyViewsLikes extends Component {
         const likes = this.props.vlData[1];
         const taxonomy = this.props.vlData[2];
         const legendData = [];
-        const colors = [ "#EFBCD5", "#FF8B4C", "#38B3A2", "#0795C7", "#F8E963", 
-                         "#4B4644", "#FF4255", "#2E294E","#E63DCF","#131200",
+        const colors = [ "#EFBCD5", "#4B4644", "#38B3A2", "#0795C7", "#F8E963", 
+                         "#FF8B4C", "#FF4255", "#2E294E","#E63DCF","#131200",
                          "#78BC61", "#79D507", "#FFEDE1"
                         ];
 
         taxonomy.forEach( (t, ix) => {
-            let legendDatumName = String(ix) + " - " + t;
-            let legendDatum = {name: legendDatumName, symbol: {fill: colors[ix] }};
+            let legendDatumName = ix + 1 + " - " + t;
+            let legendDatum = {name: legendDatumName, symbol: {fill: colors[ix]}};
             legendData.push(legendDatum);
         });        
 
         return (
             <div className="chartTaxonomyViewsLikesContainer"> 
-                <VictoryPie data={views} colorScale={colors} />
-                <VictoryPie data={likes} colorScale={colors}/>
-                <VictoryLegend title="Views and Likes" x={20} y={10} centerTitle itemsPerRow={2} orientation={"horizontal"} gutter={20} style={{ title: {fontSize: 20 } }} data={legendData} />
+                <VictoryPie data={views} colorScale={colors} style={{ labels:{fontSize:20}} }/>
+                <VictoryPie data={likes} colorScale={colors} style={{ labels:{fontSize:20}} }/>
+                <VictoryLegend title="Views and Likes" 
+                               centerTitle x={10} y={10} 
+                               itemsPerRow={2} orientation={"horizontal"} 
+                               style={{ title: {fontSize:32 }, labels:{fontSize:18} }} 
+                               data={legendData} />
             </div>
         )
     }
