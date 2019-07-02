6184ee9b0ff1a8260b745b1c041e9316965da4d3
LuisaP <luisa.pegoraro@gmail.com>
2019-07-02 14:15:17 -0400

    cleaned up pie chart

--- client/src/components/Charts/ChartViewsLikes.js
+++ client/src/components/Charts/ChartViewsLikes.js
@@ -15,52 +15,33 @@ class ChartViewsLikes extends Component {
         let views = this.props.vlData[0]; 
         let likes = this.props.vlData[1]; 
         let taxonomy = this.props.vlData[2];
+        let pieLegend =  {fontSize:"12px",color:"#0c616e", position: 'bottom', onItemClick: {toggleDataSeries: true},  horizontalAlign: 'left', itemMargin:{horizontal:5, vertical:5}};
    
         const colors = [ "#EFBCD5", "#4B4644", "#38B3A2", "#0795C7", "#F8E963", 
                          "#FF8B4C", "#FF4255", "#2E294E","#E63DCF","#131200",
                          "#78BC61", "#79D507", "#FFEDE1"
                         ];
         
-        let optViews = { chart:{ width: 300, height:300, animations: { enabled: true, easing: 'easeinout', speed: 800} },
-                    labels: taxonomy, dataLabels: {enabled: false}, fill:{ colors: colors },
-                    title: { text: "views per category", align:"center", margin: 10, style:{ fontSize: "22px", color:"#0c616e"} },
-                    colors: colors,
-                    responsive: [{  
-                                    breakpoint: 300, 
-                                    options: {  chart: { width: 400}, legend:{ show: false} }
-                                }]
-        };
-
-        let optLikes = {
-                        chart:{ width: 300, height:300, animations: { enabled: true, easing: 'easeinout', speed: 800} },
+        let optViews = { 
+                        chart:{ width: 200, height:200, animations: { enabled: true, easing: 'easeinout', speed: 200, animateGradually:{enabled:true, delay:1000} }},
+                        legend: {show:false}, colors: colors,
                         labels: taxonomy, dataLabels: {enabled: false}, fill:{ colors: colors },
-                        title: { text: "likes per category", align:"center", margin: 10, style:{ fontSize: "22px", color:"#0c616e"} },
-                        colors: colors,
-                        responsive: [{  
-                                        breakpoint: 300, 
-                                        options: {  chart: { width: 400}, 
-                                        legend: {fontSize:"16px", position: 'bottom', onItemClick: {toggleDataSeries: true},  horizontalAlign: 'left', itemMargin:{horizontal:5, vertical:10}} }
-                                    }]
+                        title: { text: "views per category", align:"center", margin: 10, style:{ fontSize: "22px", color:"#0c616e"} }                        
         };
 
-        
-        
-        // let legendViews = { show: false};
-        // let legendLikes = { show:true,  fontSize:"16px", position: 'bottom', onItemClick: {toggleDataSeries: true},  horizontalAlign: 'left', itemMargin:{horizontal:5, vertical:10}};
-        
-        // let optViews = JSON.parse(JSON.stringify(opt));
-        // let optLikes = JSON.parse(JSON.stringify(opt));
-
-        // optViews.title.text = "views per category";     
-        // optViews.responsive[0].options.legend = legendViews; 
-        // optLikes.title.text = "likes per category";
-        // optLikes.responsive[0].options.legend = legendLikes;
+        let optLikes = { 
+                        chart:{ width: 200, height:200, animations: { enabled: true, easing: 'easeinout', speed: 200, animateGradually:{enabled:true, delay:1000} }},
+                        legend: {show: false}, colors: colors,
+                        labels: taxonomy,
+                        dataLabels: {enabled: false}, fill:{ colors: colors },
+                        title: { text: "likes per category", align:"center", margin: 10, style:{ fontSize: "22px", color:"#0c616e"} }
+        s};
 
 
         return (
             <div className="chartViewsLikesContainer"> 
-                <ReactApexChart options={ optViews } series={ views } type="pie" width="360" />
-                <ReactApexChart options={ optLikes } series={ likes } type="pie" width="360" />
+                <ReactApexChart options={ optViews } series={ views } type="pie" />
+                <ReactApexChart options={ optLikes } series={ likes } type="pie" />
   
                 {/* <PieChart   data={views} label={"hello"} labelStyle={{ fontSize: "5px", fontFamily:"sans-serif", fill:"#121212" }} 
                             radius={42} labelPosition={112}
