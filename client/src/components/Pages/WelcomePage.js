3de255a96615815f6aedc602c57918d5fcf84586
LuisaP <luisa.pegoraro@gmail.com>
2019-07-06 15:51:11 -0400

    fixing css

--- client/src/components/Pages/WelcomePage.js
+++ client/src/components/Pages/WelcomePage.js
@@ -12,7 +12,9 @@ class WelcomePage extends Component {
             return <div className="welcomePageContainer"> 
                         <div className="welcomePageText1"> SUMMER IS HERE! </div> 
                         <div className="welcomePageText2"> Browse Esty listings by category! </div>
-                        <img src={"../../../images/lavender.jpg"} alt="lavender" className={"contentImage shadow welcomePageImageHide" }/>  
+                        <img src={"../../../images/lavender.jpg"} alt="lavender" className={"contentImage shadow welcomePageWeb" }/>  
+                        <img src={"../../../images/yellow_flowers.jpg"} alt="yellow_flowers" className={"welcomePageImageHide welcomePageWeb" }/>  
+                        <img src={"../../../images/poppy_flowers.jpg"} alt="poppy_flowers" className={"welcomePageImageHide welcomePageWeb" }/>  
                     </div>       
     }
 }
